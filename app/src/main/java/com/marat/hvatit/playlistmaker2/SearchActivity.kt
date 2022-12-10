2bd677c37c222a421031386c505c469eef5c4073
Титан ПК <decardcain21@gmail.com>
2022-12-10 23:04:45 +0300

    Homework 9

--- app/src/main/java/com/marat/hvatit/playlistmaker2/SearchActivity.kt
+++ app/src/main/java/com/marat/hvatit/playlistmaker2/SearchActivity.kt
@@ -1,11 +1,79 @@
 package com.marat.hvatit.playlistmaker2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SearchActivity : AppCompatActivity() {
+
+    private var saveEditText: String? = "????"
+
+    companion object {
+        const val EDITTEXT_TEXT = "EDITTEXT_TEXT"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val editText = findViewById<EditText>(R.id.editText)
+        val buttonClear: ImageButton = findViewById(R.id.buttonClear)
+        if (savedInstanceState != null) {
+            editText.setText(saveEditText)
+        }
+        //..............................................................
+        val simpletextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                //empty
+                clearButtonVisibility(s).also { buttonClear.visibility = it }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //empty
+                saveEditText = editText.text.toString()
+            }
+
+        }
+        editText.addTextChangedListener(simpletextWatcher)
+
+        buttonClear.setOnClickListener{
+            editText.requestFocus()
+            (this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager).hideSoftInputFromWindow(editText.windowToken, 0)
+        }
+
+        Log.e("Hashcode", SearchActivity.hashCode().toString())
+
     }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(EDITTEXT_TEXT,saveEditText)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle){
+        super.onRestoreInstanceState(savedInstanceState)
+        saveEditText = savedInstanceState.getString(EDITTEXT_TEXT)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            ImageButton.GONE
+        } else {
+            ImageButton.VISIBLE
+        }
+    }
+
 }
--- app/src/main/java/com/marat/hvatit/playlistmaker2/SettingsActivity.kt
+++ app/src/main/java/com/marat/hvatit/playlistmaker2/SettingsActivity.kt
@@ -1,11 +1,74 @@
 package com.marat.hvatit.playlistmaker2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val buttonShare: ImageButton = findViewById(R.id.button_share)
+        val buttonSupport: ImageButton = findViewById(R.id.button_support)
+        val buttonUserAgreement: ImageButton = findViewById(R.id.button_useragreement)
+
+
+        buttonShare.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                createIntent("Share")
+            }
+        })
+
+        buttonSupport.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                createIntent("Support")
+            }
+        })
+
+        buttonUserAgreement.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                createIntent("UserAgreement")
+            }
+        })
+
+    }
+
+    fun createIntent(string: String) {
+        val shareIntent: Intent
+        when (string) {
+            "Share" -> {
+                val message = "https://practicum.yandex.ru/android-developer/"
+                shareIntent = Intent(Intent.ACTION_SENDTO)
+                shareIntent.data = Uri.parse("mailto:")
+                shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("yourEmail@ya.ru"))
+                shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+                startActivity(shareIntent)
+            }
+            "Support" -> {
+                val tittleSend = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+                val textSend = "Спасибо разработчикам и разработчицам за крутое приложение!"
+                shareIntent = Intent(Intent.ACTION_SENDTO)
+                shareIntent.data = Uri.parse("mailto:")
+                shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("decardcain21@gmail.com"))
+                shareIntent.putExtra(Intent.EXTRA_TITLE, tittleSend)
+                shareIntent.putExtra(Intent.EXTRA_TEXT, textSend)
+                startActivity(shareIntent)
+            }
+            "UserAgreement" -> {
+                val adress = "https://yandex.ru/legal/practicum_offer/"
+                shareIntent = Intent(Intent.ACTION_VIEW,Uri.parse(adress))
+                startActivity(shareIntent)
+            }
+            else -> {
+                Toast.makeText(this@SettingsActivity, "This button disable!", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+        }
     }
 }
